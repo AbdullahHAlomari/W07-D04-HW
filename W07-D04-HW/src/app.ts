7a7a6cf38f2a51f940f4c28a9084aa3478404f55
Abdullah Alomari <123544817+AbdullahHAlomari@users.noreply.github.com>
2023-03-02 11:56:51 +0300

    Delete W07-D04-HW directory

--- W07-D04-HW/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express, {Application} from 'express';
-import useRoutes from './routes/userRoute'
-import connectDB from './config/db'
-const app:Application = express()
-app.use(express.json())
-connectDB()
-app.use('/', useRoutes)
-let port:number = 3008
-app.listen(port,()=> console.log(`Express started ${port}`))
